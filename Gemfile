2484068832080adee618b2cf6a06e95dadcfda56
Alasdair Monk <alasdair.monk@gmail.com>
2020-04-23 18:33:26 +0100

    Move to swagger ui

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'sinatra-contrib'
+gem "sinatra-cross_origin", "~> 0.3.1"
 
 group :test, :development do
   gem 'rspec'
--- app.rb
+++ app.rb
@@ -2,10 +2,16 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require "sinatra/json"
+require 'sinatra/cross_origin'
 require 'json'
 
+configure do
+  enable :cross_origin
+end
+
+# Add two numbers together
 get "/sum" do
-  a = Integer(params["number1"]) ?? 2
-  b = Integer(params["number2"]) ?? 2
+  a = Integer(params["number1"])
+  b = Integer(params["number2"])
   json :result => a + b
 end
